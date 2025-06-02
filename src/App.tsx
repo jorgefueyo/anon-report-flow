51eb141d1b7f2f151192e2dc422ac5956c82022f
jorgefueyo <112079656+jorgefueyo@users.noreply.github.com>
2025-06-02 11:29:51 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -24,7 +23,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/anon-report-flow"> {/* 👈 Añadido */}
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/nueva-denuncia" element={<NuevaDenuncia />} />
